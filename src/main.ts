7cfc0b1e9a44113a5b37c80506565a76a49acffd
Antonio Iturriaga <91393576+AntonioCait@users.noreply.github.com>
2022-10-07 00:27:00 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {packGrid} from "./packGrid";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+  /* Calling the packGrid function and then using the outerHTML property to get the HTML of the
+  element. */
   ${packGrid().outerHTML}
   </div>  
 `
