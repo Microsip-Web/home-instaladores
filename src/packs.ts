d6df431222f9298ae74b1bfa6ffde3c9e725a86a
Antonio <91393576+AntonioCait@users.noreply.github.com>
2024-04-30 11:00:13 -0600

    2024 Release

--- src/packs.ts
+++ src/packs.ts
@@ -1,71 +1,81 @@
 //array of objects typescript
-interface Pack2022 {
+interface IPack {
 	packYear: number;
 	packName: string;
 	packImage: string;
 	packFrame: string;
 }
 
-export const packs: Pack2022[] = [
-	{
-		packYear: 2023,
-		packName: 'Microsip',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd1574a682a39ee3c7a2ff_Isotipo.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/Microsip/2023-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'CEO Móvil',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4386023e66a9271f402_CEO-M%C3%B3vil.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/CEO/2023-index-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'Sync-e',
-		packImage: 'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438e5519773791717a0_SyncE.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/Sync/2023-index-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'En Ruta',
-		packImage: 'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4384ed0f2450827bbac_Ruta.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/MER/2023-index-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'SICS – Agente Matriz',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SiCS/2023-matriz-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'SICS – Agente Sucursal',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SiCS/2023-sucursal-v2.html',
-	},
-	// {
-	// 	packYear: 2022,
-	// 	packName: 'API',
-	// 	packImage:
-	// 		'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a43837602e23414bc988_api_black_24dp%201.png',
-	// 	packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/api/api-v2.html',
-	// },
-	{
-		packYear: 2023,
-		packName: 'Servidor de Aplicaciones',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438d5a1c7b7874e0ef4_apps_white_24dp%20(1)%201(1).png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SA/2023-index-v2.html',
-	},
-	{
-		packYear: 2023,
-		packName: 'Respaldos Automáticos',
-		packImage:
-			'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd161df183d85cd5970ef5_backup_white_24dp%201.png',
-		packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/RA/2023-index-v2.html',
-	},
+export const packs: IPack[] = [
+  {
+    packYear: 2024,
+    packName: "Microsip",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd1574a682a39ee3c7a2ff_Isotipo.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/Microsip/2024-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "CEO Móvil",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4386023e66a9271f402_CEO-M%C3%B3vil.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/CEO/2024-index-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "Sync-e",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438e5519773791717a0_SyncE.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/Sync/2024-index-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "En Ruta",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4384ed0f2450827bbac_Ruta.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/MER/2024-index-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "SICS – Agente Matriz",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SiCS/2024-matriz-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "SICS – Agente Sucursal",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SiCS/2024-sucursal-v2.html",
+  },
+  // {
+  // 	packYear: 2024,
+  // 	packName: 'API',
+  // 	packImage:
+  // 		'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a43837602e23414bc988_api_black_24dp%201.png',
+  // 	packFrame: 'https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/api/2024-api-v2.html',
+  // },
+  {
+    packYear: 2024,
+    packName: "Servidor de Aplicaciones",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438d5a1c7b7874e0ef4_apps_white_24dp%20(1)%201(1).png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/SA/2024-index-v2.html",
+  },
+  {
+    packYear: 2024,
+    packName: "Respaldos Automáticos",
+    packImage:
+      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd161df183d85cd5970ef5_backup_white_24dp%201.png",
+    packFrame:
+      "https://dor1k8lp3ljt2.cloudfront.net/DescargasSoporte/RA/2024-index-v2.html",
+  },
 ];
