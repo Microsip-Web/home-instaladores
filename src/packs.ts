4d993aa6ef7ed1b3785190121cea2a28b075b705
Antonio <91393576+antonioiturr@users.noreply.github.com>
2025-05-12 16:15:45 -0600

    Update pack year and asset references for 2025 release

--- src/packs.ts
+++ src/packs.ts
@@ -8,74 +8,51 @@ interface IPack {
 
 export const packs: IPack[] = [
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "Microsip",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd1574a682a39ee3c7a2ff_Isotipo.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/Microsip/2024-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/Microsip/2025-bunny.html",
   },
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "CEO Móvil",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4386023e66a9271f402_CEO-M%C3%B3vil.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/CEO/2024-index-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/CEO/2025-index-bunny.html",
   },
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "Sync-e",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438e5519773791717a0_SyncE.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/Sync/2024-index-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/Sync/2025-index-bunny.html",
   },
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "En Ruta",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4384ed0f2450827bbac_Ruta.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/MER/2024-index-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/MER/2025-index-bunny.html",
   },
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "SICS – Agente Matriz",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/SiCS/2024-matriz-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/SiCS/2025-matriz-bunny.html",
   },
   {
-    packYear: 2024,
+    packYear: 2025,
     packName: "SICS – Agente Sucursal",
     packImage:
       "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a4394ed0f24d1327bbb1_Group%20221.png",
     packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/SiCS/2024-sucursal-bunny.html",
-  },
-  // {
-  // 	packYear: 2024,
-  // 	packName: 'API',
-  // 	packImage:
-  // 		'https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a43837602e23414bc988_api_black_24dp%201.png',
-  // 	packFrame: 'https://microsip.b-cdn.net/DescargasSoporte/api/2024-api-bunny.html',
-  // },
-  {
-    packYear: 2024,
-    packName: "Servidor de Aplicaciones",
-    packImage:
-      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/6307a438d5a1c7b7874e0ef4_apps_white_24dp%20(1)%201(1).png",
-    packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/SA/2024-index-bunny.html",
-  },
-  {
-    packYear: 2024,
-    packName: "Respaldos Automáticos",
-    packImage:
-      "https://uploads-ssl.webflow.com/62ebdd458759bdb749120f22/62fd161df183d85cd5970ef5_backup_white_24dp%201.png",
-    packFrame:
-      "https://microsip.b-cdn.net/DescargasSoporte/RA/2024-index-bunny.html",
+      "https://microsip.b-cdn.net/DescargasSoporte/SiCS/2025-sucursal-bunny.html",
   },
 ];
